0d04f9e0b8a7f3aef13441c641b4940a7beefb47
Mateus <mateusmlopestr@gmail.com>
2022-11-25 15:01:45 -0300

    philo

--- philo_utils.c
+++ philo_utils.c
@@ -31,6 +31,15 @@ long long	ft_get_time(void)
 	return ((tv.tv_sec * 1000) + (tv.tv_usec / 1000));
 }
 
+void	ft_sleep(int time_sleep)
+{
+	long long       time;
+
+	time = ft_get_time();
+	while (ft_get_time() - time < time_sleep)
+		usleep(100);
+}
+
 int	ft_check_died(t_program *prog, t_philo *philo, int *pos)
 {
 	if (ft_get_time() - philo->last > prog->die)
@@ -45,13 +54,4 @@ int	ft_check_died(t_program *prog, t_philo *philo, int *pos)
 		return (ft_died(prog, philo));
 	}
 	return (1);
-}
-
-void	ft_sleep(int time_sleep)
-{
-	long long	time;
-
-	time = ft_get_time();
-	while (ft_get_time() - time < time_sleep)
-		usleep(100);
 }
